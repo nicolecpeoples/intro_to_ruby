59873dd58a637cfdcad7ce3c7f34c05f57d3da0f
Nicole Peoples <nicolecpeoples@gmail.com>
2016-06-06 22:25:02 -0700

    names'

---
+++ assignments/names.rb
@@ -0,0 +1,12 @@
+a = {:first_name => "Michael", :last_name => "Choi"}
+b = {:first_name => "John", :last_name => "Supsupin"}
+c = {:first_name => "KB", :last_name => "Tonel"}
+d = {:first_name => "Jessie", :last_name => "De Leon"}
+e = {:first_name => "Jaybee", :last_name => "Balog"}
+names = [a, b, c, d, e]
+
+names.each do|name|
+	print "the name is "
+	name.each {|key, value| print value, " " }
+	print "\n"
+end

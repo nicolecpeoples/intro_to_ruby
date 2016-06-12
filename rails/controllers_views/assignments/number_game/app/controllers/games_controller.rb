f0bf79f587f9b45a8730f5ba0f9a70b0f3ee19b3
Nicole Peoples <nicolecpeoples@gmail.com>
2016-06-12 14:31:43 -0700

    Great Number Game

---
+++ rails/controllers_views/assignments/number_game/app/controllers/games_controller.rb
@@ -0,0 +1,39 @@
+class GamesController < ApplicationController
+
+  def index
+  	if !session[:random_num] 
+  		session[:random_num] = rand(0 .. 100)
+  	end
+  	if !session[:count]
+  		session[:count] = 5
+  	end
+  	
+  	if session[:count] > 1
+	  	if request.post?
+	  		session[:count] = session[:count] - 1 
+	  		@guess = params[:guess].to_i
+	  		if @guess == session[:random_num]
+
+	  			flash[:success] = "#{session[:random_num]} was the number! <a href='#{'/games/play_again'}'>Play Again!</a>".html_safe
+	  			
+	  		elsif @guess > session[:random_num]
+	  			flash[:notice] = "Sorry your guess was too high!"
+	  			
+	  		else
+	  			flash[:notice] = "Sorry your guess was too low!"
+				
+	  		end
+	  	end
+	  else
+	 	flash[:notice] = "Sorry you've already guessed 5 times. The correct answer was #{session[:random_num]} "
+	 	
+	  end
+
+	 
+	 end
+
+  def play_again
+  	session.destroy
+  	redirect_to '/'
+  end
+end
